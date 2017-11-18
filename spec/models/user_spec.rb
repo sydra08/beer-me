330facf18fee13603ad350040a8c64cefb56abf6
github username <schun08@gmail.com>
2017-11-18 16:01:18 -0500

    outline User model validation tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    
+    describe "test factory" do
+      it "has a valid user factory with a first_name, last_name, email, and password"
+    end
+
+    describe "email" do
+      it "is invalid without an email"
+      it "has a unique email address"
+      it "is invalid without proper email format (ex. user@gmail.com)"
+    end
+
+    describe "password" do
+      it "is invalid without a password"
+      it "is invalid if the password is less than 8 characters"
+      it "encrypts the user password"
+    end
+  end
+
+  describe "relationships" do
+    it "has many user_beers"
+    it "has many beers through user_beers"
+  end
+
+  describe "class methods" do
+  end
+
+  describe "instance methods" do
+  end
+
 end
