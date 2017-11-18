70f708532ce91094534b6238e3275e4e2d367955
github username <schun08@gmail.com>
2017-11-18 16:19:03 -0500

    add User model validation tests

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,14 @@
+require 'faker'
+
+# may need to use the aliasing and association mentioned in this doc (http://www.rubydoc.info/gems/factory_girl/file/GETTING_STARTED.md)
+
 FactoryGirl.define do
   factory :user do
-    first_name "MyString"
-    last_name "MyString"
-    email "MyString"
-    password_digest "MyString"
+    first_name {Faker::Name.first_name}
+    last_name {Faker::Name.last_name}
+    email {Faker::Internet.email}
+    password {Faker::Internet.password(8,16)}
   end
 end
+
+#can use FactoryGirl to generate several test users at once
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,21 +2,48 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe "validations" do
-    
+
     describe "test factory" do
-      it "has a valid user factory with a first_name, last_name, email, and password"
+      it "has a valid user factory with a first_name, last_name, email, and password" do
+        expect(build(:user)).to be_valid
+      end
     end
 
     describe "email" do
-      it "is invalid without an email"
-      it "has a unique email address"
-      it "is invalid without proper email format (ex. user@gmail.com)"
+      it "is invalid without an email" do
+        expect(build(:user, email: nil)).not_to be_valid
+      end
+
+      it "has a unique email address" do
+        create(:user, email: "test@example.com")
+        user = build(:user, email: "test@example.com")
+
+        expect(user).not_to be_valid
+        expect(user.errors.full_messages).to include("Email has already been taken")
+
+      end
+
+      it "is invalid without proper email format (ex. user@gmail.com)" do
+        expect(build(:user, email: "test.com")).not_to be_valid
+      end
+
     end
 
     describe "password" do
-      it "is invalid without a password"
-      it "is invalid if the password is less than 8 characters"
-      it "encrypts the user password"
+      it "is invalid without a password" do
+        expect(build(:user, password: nil)).not_to be_valid
+      end
+
+      it "is invalid if the password is less than 8 characters" do
+        expect(build(:user, password: "abc")).not_to be_valid
+        expect(build(:user, password: "abcdefgh")).to be_valid
+      end
+
+      it "encrypts the user password" do
+        user = build(:user).save
+        expect(user.password_digest).not_to eq(user.password)
+      end
+
     end
   end
 
