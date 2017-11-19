0cda057b291b535f316948e4d42e34a3a769fec2
github username <schun08@gmail.com>
2017-11-19 18:43:15 -0500

    add private methods for user auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,22 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :current_user
+
+  private
+
+  def require_login
+    unless logged_in?
+      flash[:error] = "You must be logged in to do that"
+      redirect_to root_path
+    end
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def current_user
+    @user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
 end
