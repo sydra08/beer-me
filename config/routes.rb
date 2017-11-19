a47e9b8bfda12fe7ff7ed7f616935f95075ba270
github username <schun08@gmail.com>
2017-11-19 16:56:32 -0500

    generate Breweries Controller and set up initial views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :breweries
   resources :sessions, only: [:new, :create, :destroy]
   resources :categories, only: [:index, :show]
   resources :users
