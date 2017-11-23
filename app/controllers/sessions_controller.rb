0c59798ef959cd715c9f851100cfd03be8f44b38
github username <schun08@gmail.com>
2017-11-23 14:12:40 -0500

    add flash message to sign in page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,20 +1,22 @@
 class SessionsController < ApplicationController
+  before_action :require_login, only: [:destroy]
 
   def new
   end
 
   def create
+    # if the user doesn't exist then you need to render new
     # find the user by the email address
     @user = User.find_by(email: params[:user][:email])
     # check if password is correct
-    if @user.authenticate(params[:user][:password])
+    if !@user.nil? && @user.authenticate(params[:user][:password])
       # if password is correct
       # set session
       session[:user_id] = @user.id
       # redirect to user homepage
       redirect_to user_path(@user)
     else
-      render :new
+      render :new, notice: "Invalid email address"
     end
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :require_login, only: [:show]
+
   def show
     @user = User.find_by(id: params[:id])
   end
