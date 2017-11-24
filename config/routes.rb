43980c825edb65f96ed361b3db90ca437377b44f
github username <schun08@gmail.com>
2017-11-24 13:52:17 -0500

    change session routes to signin and signout

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
 
   root to: 'welcome#home'
+
+  # sessions
+  get '/signin', to: 'sessions#new'
+  post '/sessions', to: 'sessions#create'
   delete '/signout', to: 'sessions#destroy'
+
 
   resources :beers, only: [:show, :new, :create]
   resources :breweries, only: [:index, :show]
-  resources :sessions, only: [:new, :create]
   resources :categories, only: [:index, :show]
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
