f9f7807e67be736cb1ebac2dff74a0c87cc29cac
github username <schun08@gmail.com>
2017-11-24 13:48:15 -0500

    add logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -21,6 +21,8 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    session.delete(:user_id)
+    redirect_to root_path, notice: "Successfully logged out"
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
 
   root to: 'welcome#home'
+  delete '/signout', to: 'sessions#destroy'
 
   resources :beers, only: [:show, :new, :create]
   resources :breweries, only: [:index, :show]
-  resources :sessions, only: [:new, :create, :destroy]
+  resources :sessions, only: [:new, :create]
   resources :categories, only: [:index, :show]
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
