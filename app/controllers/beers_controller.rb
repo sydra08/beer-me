d4398e8f964aa378da66d79bd54b285430b5eb83
github username <schun08@gmail.com>
2017-11-26 19:17:14 -0500

    fix beers#index to display proper info based on route

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -8,13 +8,13 @@ class BeersController < ApplicationController
     # this is for the nested route
     if params[:user_id]
       if !params[:category].blank? && !params[:brewery].blank?
-        @beers = Beer.by_user_and_brewery_and_category(params[:user_id], params[:brewery], params[:category])
+        @userbeers = Beer.by_user_and_brewery_and_category(params[:user_id], params[:brewery], params[:category])
       elsif !params[:category].blank?
-        @beers = Beer.by_user_and_category(params[:user_id], params[:category])
+        @userbeers = Beer.by_user_and_category(params[:user_id], params[:category])
       elsif !params[:brewery].blank?
-        @beers = Beer.by_user_and_brewery(params[:user_id], params[:brewery])
+        @userbeers = Beer.by_user_and_brewery(params[:user_id], params[:brewery])
       else
-        @beers = User.find_by(id: params[:user_id]).beers
+        @userbeers = User.find_by(id: params[:user_id]).beers
       end
     else
       # for regular index page
