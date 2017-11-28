c57358f33fc196a6084fc00c7384149f181f61c4
github username <schun08@gmail.com>
2017-11-27 22:49:30 -0500

    figure out what data is returned by omniauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,18 +7,31 @@ class SessionsController < ApplicationController
   def create
     # if the user doesn't exist then you need to render new
     # find the user by the email address
-    raise params.inspect
-    @user = User.find_by(email: params[:user][:email])
-    # also
-    # check if password is correct
-    if !@user.nil? && @user.authenticate(params[:user][:password])
-      # if password is correct
-      # set session
+    # request.env['omniauth.auth']
+    # auth['info']['email'] => "sydneycstest@gmail.com"
+    # auth['info']['first_name'] => "Sydney"
+    # auth['info']['image'] => "https://lh3.googleusercontent.com/-XdUIqdMkCWA/AAAAAAAAAAI/AAAAAAAAAAA/4252rscbv5M/photo.jpg"
+    # auth['info']['last_name'] => "Chun"
+    # auth['provider'] => "google_oauth2"
+    # auth['uid'] => "114605032996416049559"
+    if !auth.nil?
+      raise params.inspect
+      # did the user use OAuth?
+      @user = User.find_or_create_from_auth_hash(auth)
       session[:user_id] = @user.id
-      # redirect to user homepage
       redirect_to user_path(@user)
     else
-      redirect_to new_session_path, notice: "Error: Invalid email address or password"
+      # if not sign in the normal way
+      @user = User.find_by(email: params[:user][:email])
+      if !@user.nil? && @user.authenticate(params[:user][:password])
+        # if password is correct
+        # set session
+        session[:user_id] = @user.id
+        # redirect to user homepage
+        redirect_to user_path(@user)
+      else
+        redirect_to new_session_path, notice: "Error: Invalid email address or password"
+      end
     end
   end
 
@@ -27,4 +40,9 @@ class SessionsController < ApplicationController
     redirect_to root_path, notice: "Successfully logged out"
   end
 
+  private
+
+  def auth
+    request.env['omniauth.auth']
+  end
 end
