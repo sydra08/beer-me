98972c7e8a59749520600c33cb21a0c0cfd13fd2
github username <schun08@gmail.com>
2018-03-31 21:29:36 -0400

    make beer list load after page load instead of just via button

--- app/assets/javascripts/beers.js
+++ app/assets/javascripts/beers.js
@@ -1,11 +1,22 @@
 $(function(){
   allBeersBtn();
+  applyFilterBtn();
+  getBeers();
 })
 
 function allBeersBtn() {
   // when you click on the button it invokes getBeers()
   $('#beers').on("click", function(e){
     e.preventDefault();
+    getBeers();
+  })
+}
+
+function applyFilterBtn() {
+  // submitting a form now so handle it that way
+  $('#beerFilter').on("submit", function(e){
+    e.preventDefault();
+    // need to make sure to pass through the filter param
     getBeers();
   })
 }
@@ -22,11 +33,13 @@ function getBeers() {
 function displayBeers(beerData) {
   beerData.forEach(function(beer){
     let breweryName = beer.brewery.name;
+    let breweryId = beer.brewery.id;
     let beerName = beer.name;
     let beerAbv = beer.abv;
+    let beerId = beer.id;
     // need to add the beer data to the DOM now
     // so need to figure out how to add all of the beers instead of just the last one
-    $('tbody').append(`<tr><td id="breweryName">${breweryName}</td><td id="beerName">${beerName}</td><td id="abv">${beerAbv}</td></tr>`);
+    $('tbody').append(`<tr><td id="breweryName"><a href="/breweries/${breweryId}">${breweryName}</a></td><td id="beerName"><a href="/beers/${beerId}">${beerName}</a></td><td id="abv">${beerAbv}</td></tr>`);
     // console.log(`<tr><td id="breweryName">${breweryName}</td><td id="beerName">${beerName}</td><td id="abv">${beerAbv}</td></tr>`);
   })
 }
