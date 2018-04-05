5cf7a6955f194b52dbf8c6c40b097725a53d8ff5
github username <schun08@gmail.com>
2018-04-04 21:01:28 -0400

    add conditional to breweries.js so that the right lists load on index vs show

--- app/assets/javascripts/breweries.js
+++ app/assets/javascripts/breweries.js
@@ -2,10 +2,14 @@
 $(function(){
   // this doesn't even hit debugger when you load the page
   // make call to api when filter changes
-  categoryFilterChange();
-  // make the call to GET /beers
-  // need to make sure that the getBeers() gets the right id on the page load
-  getBreweries();
+  // maybe have a function that says if it's got an id then load show page stuff?
+  // is there a way to only have the breweries load on a specific page?
+  if(window.location.pathname === "/breweries") {
+    getBreweries();
+  } else {
+    getBeers(window.location.pathname);
+  }
+
   alert("the stuff from breweries.js was loaded")
 })
 
@@ -14,9 +18,7 @@ function categoryFilterChange() {
   $('#category').on("change", function(e){
     alert("you changed a filter");
     e.preventDefault();
-    // debugger
-    // need to capture the brewery too so that you have the id to pass to the controller
-    let url = $("form").attr("action")
+    let url = $("form").attr("action");
     let category = $("#category option:selected").val();
     let formData = {category: category}
     getBeers(url, formData);
@@ -26,11 +28,11 @@ function categoryFilterChange() {
 function getBreweries() {
   $.get("/breweries", function(data){
     let breweryData = data;
-    $('tbody').empty()
+    $('tbody').empty();
     breweryData.forEach(function(brewery){
       let breweryName = brewery.name;
       let breweryLocation = brewery.location;
-      let breweryId = brewery.id
+      let breweryId = brewery.id;
       $('tbody').append(`<tr><td id="breweryName"><a href="/breweries/${breweryId}">${breweryName}</a></td><td id="breweryLocation">${breweryLocation}</td></tr>`);
     })
   })
@@ -46,17 +48,15 @@ function getBeers(url, category) {
     displayBeers(data);
   });
 }
-//
+
 function displayBeers(beerData) {
   // clear out the beer list before you add new stuff to DOM so that filters work properly
-  $('tbody').empty()
+  $('tbody').empty();
   // when the list is empty it should show you "no results"
   if (beerData.length === 0) {
     $('tbody').append("<tr><td><em>No results</em></td><td></td><td></td></tr>");
   } else {
     beerData.forEach(function(beer){
-      // let breweryName = beer.brewery.name;
-      // let breweryId = beer.brewery.location;
       let beerName = beer.name;
       let beerAbv = beer.abv;
       let beerId = beer.id;
