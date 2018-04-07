e001d57a144193819f57556e9e6699837736ba1b
github username <schun08@gmail.com>
2018-04-07 17:23:05 -0400

    clicking PREV button on breweries#show fetches brewery and renders it w/o full page refresh

--- app/assets/javascripts/breweries.js
+++ app/assets/javascripts/breweries.js
@@ -6,11 +6,42 @@ $(function(){
     console.log("the stuff for breweries#index was loaded");
   } else if (window.location.pathname.startsWith("/breweries/")) {
     getBeers(window.location.pathname);
+    prevBreweryBtn();
+    nextBreweryBtn();
     categoryFilterChange();
     console.log("the stuff for breweries#show was loaded");
   }
   console.log("the stuff from breweries.js was loaded")
 })
+
+function prevBreweryBtn(){
+  $(".js-prev").on("click", function(){
+    alert("you clicked on previous brewery");
+    let prevId = parseInt($("header.align-center").attr("data-id"))-1;
+    $.get("/breweries/" + prevId, function(data){
+      console.log(data);
+      let url = `/breweries/${prevId}`;
+      $("#breweryName").text(data[0].brewery.name);
+      $("#breweryLocation").text(data[0].brewery.location);
+      $("#breweryDescription").text(data[0].brewery.description);
+      // update data-id
+      $("header.align-center").attr("data-id", prevId)
+      getBeers(url);
+    })
+  });
+}
+
+function nextBreweryBtn(){
+  $(".js-next").on("click", function(){
+    alert("you clicked on next brewery");
+    let nextId = parseInt($("header.align-center").attr("data-id"))+1;
+    $.get("/breweries/" + nextId, function(data){
+      console.log(data);
+      // the data that's being returned includes everything - want to constrain so that it's just about breweries
+    })
+  });
+}
+
 
 function categoryFilterChange() {
   // this works and you don't have to worry about the apply filter button
